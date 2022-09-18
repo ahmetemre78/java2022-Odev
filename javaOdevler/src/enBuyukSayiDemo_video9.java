aa621fc368c7bcafbccefe1828df665c57005d18
Ahmet Emre Çetin <cetinahmetemre@gmail.com>
2022-09-18 17:31:00 +0300

    8,9.videolar uygulandı

---
+++ javaOdevler/src/enBuyukSayiDemo_video9.java
@@ -0,0 +1,25 @@
+
+public class enBuyukSayiDemo_video9 {
+
+	public static void main(String[] args) {
+		int sayi1 = 20;
+		int sayi2 = 254;
+		int sayi3 = 22;
+		
+		int enBuyukSayi = sayi1;
+		
+		
+		if(enBuyukSayi < sayi2)
+		{
+			enBuyukSayi = sayi2;
+		}
+		if(enBuyukSayi < sayi3)
+		{
+			enBuyukSayi = sayi3;
+		}
+		System.out.println("En Büyük Sayı : " + enBuyukSayi);
+		
+
+	}
+
+}
---
+++ javaOdevler/src/ifBloklari_video8.java
@@ -0,0 +1,44 @@
+
+public class ifBloklari_video8 {
+
+	public static void main(String[] args) {
+		int sayi = 68;
+		
+		if(sayi<60)
+		{
+			System.out.println("Sayı 70'den küçüktür!");
+		}
+		
+		/*if(sayi<80)
+		{
+			System.out.println("Sayı 80'den küçüktür!");
+		}*/
+		
+		int sayi2 = 22;
+		if(sayi2 < 20)
+		{
+			System.out.println("Sayı 20'den küçüktür!");
+		}
+		else
+		{
+			System.out.println("Sayı 20'den büyüktür!");
+
+		}
+		
+		int sayi3 = 70;
+		if(sayi3>70)
+		{
+			System.out.println("Sayı 70'den büyüktür!");
+		}
+		else if (sayi3==70)
+		{
+			System.out.println("İki sayı birbirine eşittir!");
+		}
+		else
+		{
+			System.out.println("Sayı 70'den küçüktür!");
+		}
+		
+	}
+
+}
